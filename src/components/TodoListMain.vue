a11147b4d25134558ae5ec8915360791835892f9
ahracho <ahra123@i-commtech.com>
2022-04-06 17:57:35 +0900

    TodoList 구조 틀 잡기

---
+++ src/components/TodoListMain.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>TODO LIST</h1><br>
+
+    <input type="text" placeholder="입력해주세요.">
+    <button>확인</button><br>
+
+    <ul>
+      <li>1번 하기 <button>완료</button></li>
+      <li>2번 하기 <button>완료</button></li>
+      <li>3번 하기 <button>완료</button></li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList"
+}
+</script>
+
+<style scoped>
+
+</style>
